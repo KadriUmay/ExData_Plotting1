928071b4be554317240a44b65f5f3aaa38b11033
KadriUmay <kadriu@umayhome.com>
2015-07-14 16:06:16 -0700

    Upload 2

---
+++ plot3.R
@@ -0,0 +1,15 @@
+source(".\\load_Data.R")
+png(filename = "plot3.png",  
+    width = 480, height = 480,  
+    units = "px", bg = "white") 
+plot(data$DateTime, data$Sub_metering_1,  
+     type = "l", 
+     col = "black", 
+     xlab = "", ylab = "Energy sub metering") 
+lines(data$DateTime, data$Sub_metering_2, col = "red") 
+lines(data$DateTime, data$Sub_metering_3, col = "blue") 
+legend("topright",  
+       col = c("black", "red", "blue"), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lwd = 1) 
+dev.off()
